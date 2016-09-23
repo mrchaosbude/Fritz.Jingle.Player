d8953c381d85f152e7c4623c3c3fdf5db0005bd9
MrChaosbude <mail@chaosbude.com>
2016-09-23 20:39:30 +0200

    add some comments

--- scrape_jingle.py
+++ scrape_jingle.py
@@ -9,10 +9,11 @@ def scrapy():
     theurl ="http://www.fritz.de/sehen-und-hoeren/jingles/index.html"
     thepage = urllib.request.urlopen(theurl)
     soup = BeautifulSoup(thepage, "html.parser")
-    for link in soup.find_all("a", {"class": "ico ico_download"}):
+
+    for link in soup.find_all("a", {"class": "ico ico_download"}): #sucht die mp3 url
         theLink.append (link['href'])
 
-    for name in soup.find_all("span", {"class": "manualteasertitle"}):
+    for name in soup.find_all("span", {"class": "manualteasertitle"}): #sucht den zur datei gehörigen namen raus
         thetext.append (name.text)
     thetext.pop(0) #der erste eintrag wird entfernt da er nicht benötigt wird
     nu = 28
